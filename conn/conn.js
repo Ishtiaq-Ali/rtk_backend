66278a853aa811f9407d44404c4c8b5ecfec4826
ishtiaq <ishtiaq.ties@gmail.com>
2024-10-01 11:18:46 +0500

    .env file add

--- conn/conn.js
+++ conn/conn.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const conn = async () => {
   try {
     await mongoose.connect(
-      process.env.MONGO_URL
+      "mongodb+srv://user:pakistan12345@cluster0.bysaq.mongodb.net/mernrtk"
     );
     console.log("MongoDB connected successfully...");
   } catch (error) {
